1f4e148a00cfd374cccaef1924e35a6a1f48e8a8
Zainul Abideen <zainul.abideen.fzd@gmail.com>
2025-02-25 19:56:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // Function to trigger when the "Make a Wish!" button is clicked
 function sendWish() {
-    document.getElementById("birthday-message").innerHTML = "Hope all your wishes come true! 🎂🎈";
+    document.getElementById("birthday-message").innerHTML = "Hope all your wishes come true! Abru Aslam 🎂🎈";
     document.getElementById("birthday-message").style.animation = "none"; // Reset animation
     document.getElementById("birthday-message").style.opacity = "1"; // Make it visible instantly after animation
 }
